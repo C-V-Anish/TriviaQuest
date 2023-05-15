1371611544a1a42f1d0ac0c9a88007412579ac3b
C.V.Anish <mahadevrao77@gmail.com>
2023-05-15 20:54:40 +0530

    Deployment

--- TriviaQuest/settings.py
+++ TriviaQuest/settings.py
@@ -147,9 +147,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 STATIC_URL = 'staticfiles/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
