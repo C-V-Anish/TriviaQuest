c5c8ac56e0d583ff13018d098e3a16625c88716e
C.V.Anish <mahadevrao77@gmail.com>
2023-05-15 20:15:17 +0530

    Deployment

--- TriviaQuest/settings.py
+++ TriviaQuest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@==7psu=-zl!06)q$a03lfu8zjk!=twkq&pwla_z*qsc%$9)44
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -141,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
---
+++ build_files.sh
@@ -0,0 +1,6 @@
+echo "BUILD START"
+
+python 3.10 -m pip install -r requirements.txt
+python 3.10 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
