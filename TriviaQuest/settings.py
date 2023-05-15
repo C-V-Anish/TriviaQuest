bacf4481652b8b826637885a0fedb0aae9d0c977
C.V.Anish <mahadevrao77@gmail.com>
2023-05-15 22:17:52 +0530

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- TriviaQuest/settings.py
+++ TriviaQuest/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['https://triviaquest-production.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://triviaquest-production.up.railway.app']
+ALLOWED_HOSTS = ['triviaquest-production.up.railway.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['triviaquest-production.up.railway.app']
 
 
 # Application definition
